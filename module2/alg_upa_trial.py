89391c9dc383e18dd91ae974523b1873d68dd7c5
Yuri Bochkarev <baltazar.bz@gmail.com>
2014-09-13 12:34:46 +0400

    module2: add provided code, data and application2 problem description

---
+++ module2/alg_upa_trial.py
@@ -0,0 +1,59 @@
+"""
+Provided code for application portion of module 2
+
+Helper class for implementing efficient version
+of UPA algorithm
+"""
+
+import random
+
+
+class UPATrial:
+    """
+    Simple class to encapsulate optimizated trials for the UPA algorithm
+
+    Maintains a list of node numbers with multiple instance of each number.
+    The number of instances of each node number are
+    in the same proportion as the desired probabilities
+
+    Uses random.choice() to select a node number from this list for each trial.
+    """
+
+    def __init__(self, num_nodes):
+        """
+        Initialize a UPATrial object corresponding to a
+        complete graph with num_nodes nodes
+
+        Note the initial list of node numbers has num_nodes copies of
+        each node number
+        """
+        self._num_nodes = num_nodes
+        self._node_numbers = [node for node in range(num_nodes) for dummy_idx in range(num_nodes)]
+
+    def run_trial(self, num_nodes):
+        """
+        Conduct num_nodes trials using by applying random.choice()
+        to the list of node numbers
+
+        Updates the list of node numbers so that each node number
+        appears in correct ratio
+
+        Returns:
+        Set of nodes
+        """
+
+        # compute the neighbors for the newly-created node
+        new_node_neighbors = set()
+        for _ in range(num_nodes):
+            new_node_neighbors.add(random.choice(self._node_numbers))
+
+        # update the list of node numbers so that each node number
+        # appears in the correct ratio
+        self._node_numbers.append(self._num_nodes)
+        for dummy_idx in range(len(new_node_neighbors)):
+            self._node_numbers.append(self._num_nodes)
+        self._node_numbers.extend(list(new_node_neighbors))
+
+        #update the number of nodes
+        self._num_nodes += 1
+        return new_node_neighbors
