783f482ede193ed69c6af66d1a3f9632b8794e93
Yuri Bochkarev <baltazar.bz@gmail.com>
2014-09-30 23:55:45 +0400

    module3: print path to data and running time in profile.py

--- module3/profile.py
+++ module3/profile.py
@@ -1,3 +1,22 @@
+import sys
 import alg_project3_solution as sol
-s = sol.Cluster.load_as_list('data/unifiedCancerData_111.csv')
-sol.hierarchical_clustering(s, 9)
+
+path = 'data/unifiedCancerData_111.csv'
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+
+s = sol.Cluster.load_as_list(path)
+
+
+def hier():
+    sol.hierarchical_clustering(s, 9)
+
+
+def kmeans():
+    sol.kmeans_clustering(s, 9, 10)
+
+
+from timeit import timeit
+print('data %s' % path)
+print('hier %s' % timeit('hier()', setup='from __main__ import hier', number=1))
+print('kmeans %s' % timeit('kmeans()', setup='from __main__ import kmeans', number=1))
